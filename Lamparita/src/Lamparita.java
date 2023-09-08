d2be178d8b9bcebf8084d4a3b3c7aff3023f19db
Marcelo Damian Valeiras <mvaleiras@bna.com.ar>
2023-09-08 17:41:15 -0300

    mensaje_del_commit

--- Lamparita/src/Lamparita.java
+++ Lamparita/src/Lamparita.java
@@ -1,4 +1,6 @@
 public class Lamparita {
+    private boolean estado;
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Vamos a programar una lamparita! ");
     }
